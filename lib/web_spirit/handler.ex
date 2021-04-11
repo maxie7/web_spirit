3da36f164caefd93dab134f950ede71430a53cfa
maxie7 <maxie7@protonmail.com>
2021-04-11 21:12:26 +0200

    Saving state

--- lib/web_spirit/handler.ex
+++ lib/web_spirit/handler.ex
@@ -1,7 +1,13 @@
 defmodule WebSpirit.Handler do
   def handle(request) do
-    request |> parse |> route |> format_response
+    request
+    |> parse
+    |> log
+    |> route
+    |> format_response
   end
+
+  def log(conv), do: IO.inspect(conv)
 
   def parse(request) do
     [method, path, _] =
@@ -13,7 +19,11 @@ defmodule WebSpirit.Handler do
   end
 
   def route(conv) do
-    %{ conv | resp_body: "Bears, Lions, Tigers" }
+    if conv.path == "/wildthings" do
+      %{ conv | resp_body: "Bears, Lions, Tigers" }
+    else
+      %{ conv | resp_body: "Teddy, Paddington, Smokey" }
+    end
   end
 
   def format_response(conv) do
@@ -29,6 +39,18 @@ end
 
 request = """
 GET /wildthings HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = WebSpirit.Handler.handle(request)
+
+IO.puts(response)
+
+request = """
+GET /bears HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
