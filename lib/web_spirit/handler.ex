37d81cd53764ee5dd3dc0a17a358de086fde335d
maxie7 <maxie7@protonmail.com>
2021-04-15 22:46:49 +0200

    Add >> Rewrite paths && tracks

--- lib/web_spirit/handler.ex
+++ lib/web_spirit/handler.ex
@@ -2,10 +2,25 @@ defmodule WebSpirit.Handler do
   def handle(request) do
     request
     |> parse
+    |> rewrite_path
     |> log
     |> route
+    |> track
     |> format_response
   end
+
+  def track(%{status: 404, path: path} = conv) do
+    IO.puts "Warning: #{path} is on the loose!"
+    conv
+  end
+
+  def track(conv), do: conv
+
+  def rewrite_path(%{path: "/wildlife"} = conv) do
+    %{ conv | path: "/wildthings" }
+  end
+
+  def rewrite_path(conv), do: conv
 
   def log(conv), do: IO.inspect(conv)
 
@@ -22,23 +37,24 @@ defmodule WebSpirit.Handler do
     }
   end
 
-  def route(conv) do
-    route(conv, conv.method, conv.path)
-  end
+#  def route(conv) do
+#    route(conv, conv.method, conv.path)
+#  end
 
-  def route(conv, "GET", "/wildthings") do
+  def route(%{ method: "GET", path: "/wildthings" } = conv) do
+    # Example >>> " %{conv | resp_body: "Bears" " is a alternative to " Map.put(conv, :resp_body, "Bears") "
     %{ conv | status: 200, resp_body: "Bears, Lions, Tigers" }
   end
 
-  def route(conv, "GET", "/bears") do
+  def route(%{ method: "GET", path: "/bears" } = conv) do
     %{ conv | status: 200, resp_body: "Teddy, Paddington, Smokey" }
   end
 
-  def route(conv, "GET", "/bears/" <> id) do
+  def route(%{ method: "GET", path: "/bears" <> id } = conv) do
     %{ conv | status: 200, resp_body: "Bear #{id}" }
   end
 
-  def route(conv, _method, path) do
+  def route(%{ path: path } = conv) do
     %{ conv | status: 404 , resp_body: "No #{path} here!" }
   end
 
@@ -99,6 +115,18 @@ IO.puts(response)
 
 request = """
 GET /bears/1 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = WebSpirit.Handler.handle(request)
+IO.puts(response)
+
+
+request = """
+GET /wildlife HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
