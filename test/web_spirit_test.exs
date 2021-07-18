7c910e90a5d42e1fa6b412c84b146cf14a58ae51
maxie7 <maxie7@protonmail.com>
2021-07-18 17:23:31 +0200

    Add tests && doctests

--- test/web_spirit_test.exs
+++ test/web_spirit_test.exs
@@ -2,7 +2,7 @@ defmodule WebSpiritTest do
   use ExUnit.Case
   doctest WebSpirit
 
-  test "greets the world" do
-    assert WebSpirit.hello() == :world
+  test "the truth" do
+    assert 1 + 1 == 2
   end
 end
