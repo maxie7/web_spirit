8a459d2d37c9a685fee7627e1f002ed4eafe0a64
maxie7 <maxie7@protonmail.com>
2021-08-05 20:41:46 +0200

    Add listen_loop func

--- lib/web_spirit/pledge_server.ex
+++ lib/web_spirit/pledge_server.ex
@@ -1,15 +1,32 @@
 defmodule WebSpirit.PledgeServer do
-  def create_pledge(name, amount) do
-    {:ok, id} = send_pledge_to_service(name, amount)
+  def listen_loop(state) do
+    IO.puts "\nWaiting for a message..."
 
-    # Cache the pledge:
-    [ {"pepe", 10} ]
+    receive do
+      {:create_pledge, name, amount} ->
+        {:ok, id} = send_pledge_to_service(name, amount)
+        new_state = [ {name, amount} | state ]
+        IO.puts "#{name} pledged #{amount}!"
+        IO.puts "New state is #{inspect new_state}"
+        listen_loop(new_state)
+      {sender, :recent_pledges} ->
+        send sender, {:response, state}
+        IO.puts "Sent pledges to #{inspect sender}"
+        listen_loop(state)
+    end
   end
 
-  def recent_pledges do
-    # Returns the most recent pledges (cache):
-    [ {"pepe", 10} ]
-  end
+#  def create_pledge(name, amount) do
+#    {:ok, id} = send_pledge_to_service(name, amount)
+#
+#    # Cache the pledge:
+#    [ {"pepe", 10} ]
+#  end
+#
+#  def recent_pledges do
+#    # Returns the most recent pledges (cache):
+#    [ {"pepe", 10} ]
+#  end
 
   def send_pledge_to_service(_name, _amount) do
     # Code goes here to send pledge to external service
