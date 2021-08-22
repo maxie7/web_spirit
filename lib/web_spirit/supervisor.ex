dcaf27d7ccdb2e73836a1c91c5f202f1083cb61b
maxie7 <maxie7@protonmail.com>
2021-08-22 14:26:20 +0200

    Add top level supervisor

--- lib/web_spirit/supervisor.ex
+++ lib/web_spirit/supervisor.ex
@@ -1,15 +1,15 @@
-defmodule WebSpirit.ServicesSupervisor do
+defmodule WebSpirit.Supervisor do
   use Supervisor
 
-  def start_link(_arg) do
-    IO.puts "Starting the services supervisor..."
+  def start_link do
+    IO.puts "Starting the top level supervisor..."
     Supervisor.start_link(__MODULE__, :ok, name: __MODULE__)
   end
 
   def init(:ok) do
     children = [
-      WebSpirit.PledgeServer,
-      {WebSpirit.SensorServer, 60}
+      WebSpirit.KickStarter,
+      WebSpirit.ServicesSupervisor
     ]
 
     Supervisor.init(children, strategy: :one_for_one)
